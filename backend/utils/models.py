315eb13050f73e00b552c92c9fdc3aca2d3ad8d8
Sainyamcc <sainyam.chandra@crowdbotics.com>
2022-01-20 12:18:42 +0100

    fix: removing the gis module as it is not needed for now

--- backend/utils/models.py
+++ backend/utils/models.py
@@ -1,7 +1,8 @@
 import uuid
 
 from django.db import models
-from django.contrib.gis.db import models as gismodels
+
+# from django.contrib.gis.db import models as gismodels
 
 
 class BaseModel(models.Model):
@@ -13,10 +14,10 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class GeoBaseModel(gismodels.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+# class GeoBaseModel(gismodels.Model):
+#     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        abstract = True
+#     class Meta:
+#         abstract = True
