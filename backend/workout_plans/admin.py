3b679c6b600de0ec24e4b54ea281dd24df8a621b
Sainyam Chandra <sainyam.chandra@crowdbotics.com>
2022-03-25 13:17:59 +0100

    feat: workout plan models added

--- backend/workout_plans/admin.py
+++ backend/workout_plans/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from workout_plans.models import WorkoutPlan
+
+admin.site.register(WorkoutPlan)
--- backend/workout_plans/models.py
+++ backend/workout_plans/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from utils.models import BaseModel
+from plans.models import Plan
+from workouts.models import Workout
+
+
+class WorkoutPlan(BaseModel):
+    workout = models.ForeignKey(
+        Workout,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="workout_plan_workout",
+    )
+    plan = models.ForeignKey(
+        Plan,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="workout_plan_plan",
+    )
+
+    class Meta:
+        verbose_name = "Workout Plans"
+        verbose_name_plural = "Workout Plans"
+
+    def __str__(self) -> str:
+        return f"Plan: {self.plan} | Workout: {self.workout}"
