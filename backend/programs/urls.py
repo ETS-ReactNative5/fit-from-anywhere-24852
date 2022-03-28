8f18e9df88feac5a2846c146620171742ad91372
Sainyam Chandra <sainyam.chandra@crowdbotics.com>
2022-03-28 09:55:34 +0200

    feat: user progress endpoints added

--- backend/programs/urls.py
+++ backend/programs/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import ProgramViewSet
+from .views import ProgramViewSet, UserProgressViewSet
 
 app_name = "programs"
 
 router = DefaultRouter()
 router.register("program", ProgramViewSet, basename="programs")
+router.register("user-progress", UserProgressViewSet, basename="user-progress")
 
 urlpatterns = [
     path("", include(router.urls)),
--- backend/programs/views.py
+++ backend/programs/views.py
@@ -1,8 +1,13 @@
 import imp
 from rest_framework import viewsets
 
-from .models import Program
-from .serializers import ProgramSerializer, GetProgramSerializer
+from .models import Program, UserProgress
+from .serializers import (
+    ProgramSerializer,
+    GetProgramSerializer,
+    UserProgressSerializer,
+    GetUserProgressSerializer,
+)
 
 
 class ProgramViewSet(viewsets.ModelViewSet):
@@ -13,3 +18,20 @@ class ProgramViewSet(viewsets.ModelViewSet):
             return ProgramSerializer
         else:
             return GetProgramSerializer
+
+
+class UserProgressViewSet(viewsets.ModelViewSet):
+    queryset = UserProgress.objects.all()
+    filterset_fields = {
+        "user__id": ["exact"],
+        "set": ["gte", "lte", "exact", "gt", "lt", "range"],
+        "repetition": ["gte", "lte", "exact", "gt", "lt", "range"],
+        "program__id": ["exact"],
+        "workout_plan__id": ["exact"],
+    }
+
+    def get_serializer_class(self):
+        if self.request.method in ("PUT", "POST", "PATCH"):
+            return UserProgressSerializer
+        else:
+            return GetUserProgressSerializer
